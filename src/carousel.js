f063324f787b42d6aa03225d476553e8e3c7954f
Yuval Saraf <yuvalsaraf@gmail.com>
2015-11-21 01:17:34 +0200

    Access to change list's and frame's styles
    I need it for my own project and it's a reasonable add-on.
    Thanks in advance.

--- src/carousel.js
+++ src/carousel.js
@@ -120,11 +120,11 @@ const Carousel = React.createClass({
       <div className={['slider', this.props.className || ''].join(' ')} ref="slider" style={assign(this.getSliderStyles(), this.props.style || {})}>
         <div className="slider-frame"
           ref="frame"
-          style={this.getFrameStyles()}
+          style={assign(this.getFrameStyles(), this.props.frameStyle || {})}
           {...this.getTouchEvents()}
           {...this.getMouseEvents()}
           onClick={this.handleClick}>
-          <ul className="slider-list" ref="list" style={this.getListStyles()}>
+          <ul className="slider-list" ref="list" style={assign(this.getListStyles(), this.props.listStyle || {})}>
             {children}
           </ul>
         </div>
